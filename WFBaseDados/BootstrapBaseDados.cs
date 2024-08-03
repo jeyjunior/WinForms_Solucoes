e18f28267e90f34fd1dbc0d19a3267e197591ec9
José Junior <jeyjuniordg@gmail.com>
2024-08-03 16:20:57 -0300

    Estrutura base de dados

---
+++ WFBaseDados/BootstrapBaseDados.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFBase.Base;
+using WFBase.Interface;
+using WFBase;
+using System.Data.Common;
+using System.IO;
+namespace WFBaseDados
+{
+    public static class BootstrapBaseDados
+    {
+        public static Container Container { get; private set; }
+        public static void Iniciar(Container container)
+        {
+            Container = container;
+
+
+
+            IniciarBaseDados();
+        }
+
+        private static void IniciarBaseDados()
+        {
+
+        }
+    }
+}
