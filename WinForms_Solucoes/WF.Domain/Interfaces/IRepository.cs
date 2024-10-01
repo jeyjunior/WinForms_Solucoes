0d8a37bd96ece9376273727ab73cbaa046515db2
José Junior <jeyjuniordg@gmail.com>
2024-09-30 21:22:40 -0300

    Estrutura Base UnityOfWork Obter Por ID

---
+++ WinForms_Solucoes/WF.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WF.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T GetById(object id);
+    }
+}
---
+++ WinForms_Solucoes/WF.Infra.Data/Repositories/Repository.cs
@@ -0,0 +1,30 @@
+﻿using JJCore.Domain.Interfaces;
+using JJCore.Infra.Data;
+using JJCore.Infra.Data.Extesions;
+using System;
+using System.Collections.Generic;
+using WF.Domain.Interfaces;
+
+namespace Gestor.GES.Infra.Data.Repositories
+{
+    public class Repository<T> : IRepository<T>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Repository(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public T GetById(object id)
+        {
+            return _unitOfWork.Connection.Obter<T>(id, _unitOfWork.Transaction);
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
